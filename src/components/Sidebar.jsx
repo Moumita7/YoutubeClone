ed8edf15fa9735af8e99c4da5b59a7efbbb2776c
Moumita Das <dasmoumita.me@gmail.com>
2023-07-20 13:55:03 +0530

    adding redux

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,31 @@
+import { AiOutlineHome } from 'react-icons/ai';
+import {BsFillCollectionPlayFill,BsCollectionPlay} from 'react-icons/bs';
+import { BiMoviePlay } from 'react-icons/bi';
+
+
+
+
 
 
 const Sidebar = () => {
   return (
-    <div className="dark:text-yellow-200">Sidebar bb</div>
+    <div className="dark:text-yellow-200 bg-red-400 w-[12rem]" >
+    <ul>
+      <li className='flex items-center gap-3'><AiOutlineHome/>Home</li>
+      <li className='flex items-center gap-3'><BsCollectionPlay/>shorts</li>
+      <li className='flex items-center gap-3'><BsFillCollectionPlayFill/>Subscriptions</li>
+    <div className="border"></div>
+    <li className='flex items-center gap-3'><BiMoviePlay/>Libary</li>
+      <li>shorts</li>
+      <li>Subscriptions</li>
+
+    </ul>
+ 
+  
+    
+
+
+    </div>
   )
 }
 
